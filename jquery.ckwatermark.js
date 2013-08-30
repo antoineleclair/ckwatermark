06ce39342a2948aab039e7354b237b392b26b24b
Antoine Leclair <antoineleclair@gmail.com>
2013-08-30 18:56:12 -0400

    Code style

--- jquery.ckwatermark.js
+++ jquery.ckwatermark.js
@@ -1,5 +1,5 @@
 // https://github.com/antoineleclair/ckwatermark
-(function ($) {
+(function($) {
 
     function _onBlur(e) {
         e.editor.updateElement();
@@ -7,16 +7,16 @@
         var val = $textArea.val();
         val.replace(/\s/g, '');
         if (val == '' || val == '<p></p>') {
-            $textArea.val($textArea.data('ckwatermark'));
+            var watermark = $textArea.data('ckwatermark');
+            $textArea.val(watermark);
             if (e.editor.document) {
-                e.editor.document.getBody().setHtml($textArea.data('ckwatermark'));
+                e.editor.document.getBody().setHtml(watermark);
             } else {
-                e.editor.setData($textArea.data('ckwatermark'));
+                e.editor.setData(watermark);
             }
             e.editor.updateElement();
             e.editor.resetDirty();
         }
-        
     }
 
     function _onFocus(e) {
@@ -26,7 +26,7 @@
         var watermark = $textArea.data('ckwatermark').replace(/\s/g, '');
         var editorval = e.editor.getData().replace(/\s/g, '');
         if (val == watermark || editorval == watermark) {
-            setTimeout(function () {
+            setTimeout(function() {
                 if (CKEDITOR.instances[id].document) {
                     CKEDITOR.instances[id]
                         .document
@@ -42,12 +42,12 @@
         }
     }
 
-    $.fn.ckwatermark = function (text) {
-        return this.each(function (i, el) {
+    $.fn.ckwatermark = function(text) {
+        return this.each(function(i, el) {
             $(this).addClass('ckwatermark');
             $(this).data('ckwatermark', text);
-            $(this).closest('form').submit(function () {
-                $(this).find('textarea.ckwatermark').each(function () {
+            $(this).closest('form').submit(function() {
+                $(this).find('textarea.ckwatermark').each(function() {
                     var editor = CKEDITOR.instances[$(this).attr('id')];
                     var val = editor.getData().replace(/\s/g, '');
                     var watermark = $(this).data('ckwatermark')
